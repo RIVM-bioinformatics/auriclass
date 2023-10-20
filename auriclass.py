cb9a1845cb5fa216f6c7026af47b0484ad97d889
boasvdp <boasvdp@gmail.com>
2023-10-20 19:32:37 +0200

    chore: remove commented section

--- auriclass.py
+++ auriclass.py
@@ -97,24 +97,6 @@ def main() -> None:
     # Run object
     sample.run()
 
-    # # Create object
-    # sample = AuriClassAnalysis(
-    #     name=args.name,
-    #     output_report_path=args.output_report_path,
-    #     read_paths=args.read_file_paths,
-    #     reference_sketch_path=args.reference_sketch_path,
-    #     kmer_size=int(args.kmer_size),
-    #     sketch_size=int(args.sketch_size),
-    #     minimal_kmer_coverage=int(args.minimal_kmer_coverage),
-    #     n_threads=int(args.n_threads),
-    #     clade_config_path=args.clade_config_path,
-    #     genome_size_range=[int(size) for size in args.expected_genome_size],
-    #     non_candida_threshold=float(args.non_candida_threshold),
-    #     new_clade_threshold=float(args.new_clade_threshold),
-    # )
-
-    # sample.run()
-
 
 if __name__ == "__main__":
     main()
